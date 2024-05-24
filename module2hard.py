c7005127137a1604a385d700d1425c0990915757
letmediealrdy <letmediealrdy@gmail.com>
2024-05-24 18:25:15 +0200

    initial

--- module2hard.py
+++ module2hard.py
@@ -1,6 +1,8 @@
 num = int(input('Введите число от 3 до 20: '))
+result = ''
 for i in range(1, 20):
     for v in range(1, 20):
         pair = i, v
         if num % sum(pair) == 0 and i < v:
-            print(i, v)
+            result += (str(i) + str(v))
+print(result)
